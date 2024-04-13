8d086cfa09a4a7d1359a59d593aa69bdfc490242
Matthhe0 <151616663+Matthhe00@users.noreply.github.com>
2024-04-13 14:35:51 +0200

    M

---
+++ Application/App/src/application/Main.java
@@ -0,0 +1,56 @@
+package application;
+    
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Image icon = new Image("Logo_SAE.png");
+        Image backgroundImage = new Image("BackGround_SAE.png");
+        BackgroundImage background = new BackgroundImage(backgroundImage,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                new BackgroundSize(100, 100, true, true, false, true));
+        
+        Pane root = new Pane();
+        root.setBackground(new Background(background));
+        
+        Scene scene = new Scene(root, 1260, 700);
+        primaryStage.getIcons().add(icon);
+        primaryStage.setTitle("Attrac'Ty Breizh");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        
+        PasswordField passwordField = new PasswordField();
+        root.getChildren().add(passwordField);
+
+        
+        Button button = new Button();
+                button.setOnAction(event -> {
+                    System.out.println(passwordField.getText());
+                });
+                
+        button.setText("Salut");
+        root.getChildren().add(button);
+    }
+}
