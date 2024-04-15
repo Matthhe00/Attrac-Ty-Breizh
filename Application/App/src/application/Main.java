650c0ee249601b19f8b0593585d8df20b771944e
Matthhe0 <jules-vachet.e2300672@etud.univ-ubs.fr>
2024-04-15 09:15:36 +0200

    truc

--- Application/App/src/application/Main.java
+++ Application/App/src/application/Main.java
@@ -3,8 +3,6 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
