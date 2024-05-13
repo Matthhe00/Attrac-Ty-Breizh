881c9dfdefa6dc48f3bde184167aba6d319b37b5
Matthhe0 <jules-vachet.e2300672@etud.univ-ubs.fr>
2024-05-13 14:46:01 +0200

    g

--- Application/AppVS/src/application/model/Modele.java
+++ Application/AppVS/src/application/model/Modele.java
@@ -1,17 +1,6 @@
 package application.model;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-
 
 public class Modele {
 
-    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
-    
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
-        changeSupport.addPropertyChangeListener(listener);
-    }
 
-    public void removePropertyChangeListener(PropertyChangeListener listener) {
-        changeSupport.removePropertyChangeListener(listener);
-    }
 }
