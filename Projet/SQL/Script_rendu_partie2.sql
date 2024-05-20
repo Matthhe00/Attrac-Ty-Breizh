56598f2f8baf0a9009b8684413166648ecad07c6
Matthhe0 <jules-vachet.e2300672@etud.univ-ubs.fr>
2024-05-20 13:13:43 +0200

    Update Script_rendu_partie2.sql

--- Projet/SQL/Script_rendu_partie2.sql
+++ Projet/SQL/Script_rendu_partie2.sql
@@ -8,15 +8,14 @@ JOIN Aeroport ON Departement.idDep = Aeroport.leDepartement
 ORDER BY Departement.idDep;
 
 /* Question 2 : auto-jointure */
--- Quels sont les 10 premières villes possédant au moins deux gares ?
-SELECT C.nomCommune, COUNT(G.codeGare) AS nombreDeGares
-FROM Commune C
-JOIN Gare G ON C.idCommune = G.laCommune
-GROUP BY C.nomCommune
-HAVING COUNT(G.codeGare) >= 2
-ORDER BY nombreDeGares DESC
-LIMIT 10;
-
+-- Quels sont les villes possédant plus d'une gares ?
+SELECT C1.nomCommune, COUNT(DISTINCT G1.codeGare) AS nombreDeGares
+FROM Commune C1
+JOIN Gare G1 ON C1.idCommune = G1.laCommune
+JOIN Gare G2 ON C1.idCommune = G2.laCommune AND G1.codeGare < G2.codeGare
+GROUP BY C1.nomCommune
+HAVING COUNT(DISTINCT G1.codeGare) >= 2
+ORDER BY nombreDeGares DESC;
 
 /* Question 3 : jointure externe */
 -- Quels sont les 10 premières communes n'ayant pas de gare ?
@@ -26,7 +25,6 @@ LEFT JOIN Gare ON Commune.idCommune = Gare.laCommune
 WHERE Gare.codeGare IS NULL
 ORDER BY Commune.idCommune
 LIMIT 10;
-
 
 /* Question 4 : jointure externe */
 
@@ -41,7 +39,7 @@ WHERE leDepartement IN (
     WHERE UPPER(nomDep) = 'ILLE-ET-VILAINE'
     );
 
--- /* Question 6 : sous-requête (avec NOT IN) */
+/* Question 6 : sous-requête (avec NOT IN) */
 -- Quels sont les gares qui ne sont pas dans le département 56 ?
 SELECT nomGare, laCommune
 FROM Gare
@@ -52,7 +50,7 @@ WHERE laCommune NOT IN (
     )
 ORDER BY codeGare;
 
--- /* Question 7 : sous-requête (avec EXISTS) */
+/* Question 7 : sous-requête (avec EXISTS) */
 -- Quels sont les communes ayant au moins 2 gares ?
 SELECT nomCommune, leDepartement
 FROM Commune c1
@@ -65,7 +63,7 @@ WHERE EXISTS (
     )
 ORDER BY c1.idCommune;
 
--- /* Question 8 : sous-requête (avec NOT EXISTS) */
+/* Question 8 : sous-requête (avec NOT EXISTS) */
 -- Quels sont les communes n'ayant pas de gare ?
 SELECT *
 FROM Commune c1
@@ -77,60 +75,78 @@ WHERE NOT EXISTS (
 ORDER BY c1.idCommune;
 
 /* Question 9 : fonction de groupe sans regroupement */
--- Calculer l'investissement culturel moyen par département.
+-- Quel est l'investissement culturel moyen par département ?
 SELECT nomDep, AVG(investissementCulturel2019) AS investissementCulturelMoyen
 FROM Departement
 GROUP BY nomDep;
 
 /* Question 10 : fonction de groupe sans regroupement */
--- Trouver le département avec le maximum d'investissement culturel.
-SELECT nomDep, investissementCulturel2019
-FROM Departement
-WHERE investissementCulturel2019 = (SELECT MAX(investissementCulturel2019) FROM Departement);
-
-/* Question 11 : jointure et fonction de groupe */
--- Nombre de communes par département.
-SELECT D.nomDep, COUNT(C.idCommune) AS nombreDeCommunes
-FROM Departement D
-JOIN Commune C ON D.idDep = C.leDepartement
-GROUP BY D.nomDep;
-
--- /* Question 12 : jointure avec condition */
-
-
--- /* Question 13 : jointure externe et fonction de groupe */
-
-
-/* Question 14 : sous-requête corrélée */
--- Liste des communes ayant plus de 2 gares.
-SELECT nomCommune
-FROM Commune C
-WHERE (SELECT COUNT(*) FROM Gare G WHERE G.laCommune = C.idCommune) > 2;
-
-/* Question 15 : jointure et fonction de groupe avec condition */
--- Nombre total d'investissement culturel pour les départements ayant plus de 300 communes.
-SELECT D.nomDep, SUM(D.investissementCulturel2019) AS investissementTotal
-FROM Departement D
-JOIN Commune C ON D.idDep = C.leDepartement
-GROUP BY D.nomDep
-HAVING COUNT(C.idCommune) > 300;
-
--- /* Question 16 : sous-requête imbriquée */
-
-
--- /* Question 17 : jointure avec agrégation et condition */
-
-
-/* Question 18 : jointure externe avec fonction de groupe */
 -- Nombre de gares par commune, incluant les communes sans gares.
 SELECT C.nomCommune, COUNT(G.codeGare) AS nombreDeGares
 FROM Commune C
 LEFT JOIN Gare G ON C.idCommune = G.laCommune
 GROUP BY C.nomCommune;
 
--- /* Question 19 : jointure multiple */
+/* Question 11 : regroupement avec fonction de groupe */
+-- Quel est le nombre de communes par département ?
+SELECT D.nomDep, COUNT(C.idCommune) AS nombreDeCommunes
+FROM Departement D
+JOIN Commune C ON D.idDep = C.leDepartement
+GROUP BY D.nomDep;
+
+/* Question 12 :regroupement avec fonction de groupe */
+-- Quel est le nombre de commune voisine trié par ordre décroissant ?
+SELECT Commune.nomCommune, COUNT(Voisinage.communeVoisine) AS nbVoisins
+FROM Voisinage
+JOIN Commune ON Voisinage.commune = Commune.idCommune
+LEFT JOIN Commune voisine ON Voisinage.communeVoisine = voisine.idCommune
+GROUP BY Commune.nomCommune
+ORDER BY nbVoisins DESC;
+
+/* Question 13 : regroupement et restriction (avec HAVING) */
+-- Quel est le nombre de gares par departement ?
+SELECT D.nomDep, COUNT(G.codeGare) AS nombreDeGares
+FROM Gare G
+LEFT JOIN Commune C ON G.laCommune = C.idCommune
+LEFT JOIN Departement D ON C.leDepartement = D.idDep
+GROUP BY D.nomDep
+HAVING COUNT(G.codeGare) > 0;
+
+/* Question 14 : regroupement et restriction (avec HAVING) */
+-- Quel est l'investissement total par département pour les départements ayant plus de 300 communes ?
+SELECT D.nomDep, SUM(D.investissementCulturel2019) AS investissementTotal
+FROM Departement D
+JOIN Commune C ON D.idDep = C.leDepartement
+GROUP BY D.nomDep
+HAVING COUNT(C.idCommune) > 300;
+
+/* Question 15 : division normale */
 
 
--- /* Question 20 : sous-requête avec agrégation */
+/* Question 16 : division exacte */
+
+
+/* Question 17 : vue (pour gérer des contraintes à proposer)  */
+-- Vue pour obtenir les départements avec moins de 300 communes
+CREATE OR REPLACE VIEW DepartementsPlusDeCentCommunes AS
+SELECT D.nomDep, COUNT(C.idCommune) AS nombreDeCommunes
+FROM Departement D
+JOIN Commune C ON D.idDep = C.leDepartement
+GROUP BY D.nomDep
+HAVING COUNT(C.idCommune) < 300;
+
+SELECT * FROM DepartementsPlusDeCentCommunes;
+
+
+/* Question 18 : vue (pour gérer des contraintes à proposer) */
+
+
+/* Question 19 : vue (pour gérer des informations d´erivables à proposer) */
+
+
+/* Question 20 : vue (pour gérer des informations d´erivables à proposer) */
+
+
+
 
 
